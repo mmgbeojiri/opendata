d38c87b8ceededc7f0f39f8b8f536d94f414bc8e
mmgbeojiri <150837937+mmgbeojiri@users.noreply.github.com>
2025-05-14 14:10:14 +0000

    added scrolling navbar

--- client/src/Home.tsx
+++ client/src/Home.tsx
@@ -4,11 +4,10 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function Home() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
+      <div className='m-5'>
         <h1>Home Page</h1>
         <p>The dataset used for this website is the 311 Service Requests from 2010 to Present.</p>
         <p>This set is provided by the <b>Social Services</b> Agency</p>
--- client/src/Navbar.tsx
+++ client/src/Navbar.tsx
@@ -1,17 +1,47 @@
 import './App.css'
 
 function Navbar() {
+  const toggleStuff = (e: React.MouseEvent<HTMLButtonElement>) => {
+
+
+    /* Toggle between hiding and showing the active panel */
+    let container = document.getElementById("panelContainer");
+    if (container) {
+    if (container.style.height !== "0px") {
+      container.style.height = "0px";
+    } else {
+      container.style.height = container.scrollHeight + "px";
+    }
+  }
+
+  }
 
   return (
     <>
-      <div className="bg-blue-300 p-2 text-white items-center flex justify-around flex-row gap-[50%]">
-        <h1 className='logo'>OpenData.com</h1>
+      <div id="navbar" className="bg-blue-300 p-2 text-white items-center flex justify-between flex-row">
+        <h1 className='navbarbutton'>OpenData.com</h1>
 
-        <div className="flex text-xl flex-row gap-5">
+        <div className="hidden lg:flex text-xl flex-row gap-5">
           <a href="#">Home</a>
-          <a href="#">Info </a>
+          <a href="#">Info</a>
           <a href="#">Analysis</a>
-          </div>
+        </div>
+
+        <button id="menuButton" className="navbarbutton hover:!bg-blue-300 !px-5 text-2xl" onClick={toggleStuff}>☰</button>
+        
+
+        
+      </div>
+      <div id="panelContainer" className='ease-out duration-200 h-0 overflow-hidden'>
+      <div className='panel'>
+        <p>Home</p>
+      </div>
+      <div className='panel'>
+        <p>Info</p>
+      </div>
+       <div className='panel'>
+        <p>Analysis</p>
+      </div>
       </div>
     </>
   )
