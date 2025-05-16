e4d210fcb05d1ed98e73ca8231719aced622b940
michaelm1464@nycstudents.net <mmgbeojirikwe@gmail.com>
2025-05-16 14:55:10 -0400

    update boroughs

--- client/src/Analysis.tsx
+++ client/src/Analysis.tsx
@@ -50,11 +50,23 @@ function Analysis() {
 
   const [boroughData, setBoroughData] = useState<ArrayEntry[]>([])
 
+  const boroughsTally: ArrayEntry[] = [
+    ["MANHATTAN", 0],
+    ["BRONX", 0],
+    ["BROOKLYN", 0],
+    ["QUEENS", 0],
+    ["STATEN ISLAND", 0]
+  ]
+
   function tallyBoroughs() {
       jsonData.forEach((entry: DataEntry) => {
         if (entry.borough) {
             console.log(entry.borough)
-            let boroughEntry: ArrayEntry = [entry.borough, 0]
+            for (let i = 0; i < boroughsTally.length; i++) {
+                if (entry.borough.toUpperCase() === boroughsTally[i][0]) {
+                    boroughsTally[i][1] += 1
+                }
+            }
            
         }
     })
@@ -67,7 +79,7 @@ function Analysis() {
       <div className='m-5'>
         <h1>Analysis</h1>
       </div>
-        {boroughData.toString()}
+        {boroughsTally.toString()}
       
     </>
   )
