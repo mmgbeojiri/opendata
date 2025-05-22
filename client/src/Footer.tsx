0904016752cb475d0f26439c28f3b5267810dff2
michaelm1464@nycstudents.net <mmgbeojirikwe@gmail.com>
2025-05-22 14:55:00 -0400

    footer

--- client/src/Footer.tsx
+++ client/src/Footer.tsx
@@ -2,7 +2,7 @@ import "./App.css"
 
 function Footer() {
     return (
-        <footer className="bg-blue-300 p-5  font-bold text-white items-center flex justify-between flex-row">
+        <footer className="bg-blue-300  w-full p-5 mt-2 font-bold text-white items-center flex justify-between flex-row">
             <span>
                 Copyright &copy; 2025 OpenData. All rights reserved.
             </span>
