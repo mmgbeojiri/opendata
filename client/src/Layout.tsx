8e3dc17f24a3d45413447a9f5c3fcfd5f7b9c226
michaelm1464@nycstudents.net <mmgbeojirikwe@gmail.com>
2025-05-23 14:50:51 -0400

    layout

---
+++ client/src/Layout.tsx
@@ -0,0 +1,19 @@
+// Layout.js
+import Navbar from './Navbar';
+import Footer from './Footer';
+import './App.css';
+import { Outlet } from 'react-router-dom';
+
+function Layout() {
+  return (
+    <>
+      <Navbar />
+      <main>
+        <Outlet />
+      </main>
+        <Footer />
+    </>
+  );
+}
+
+export default Layout;
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,24 +1,23 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Home from './Home.tsx'
-import Navbar from './Navbar.tsx'
+import Layout from './Layout.tsx'
 import Info from './Info.tsx'
 import Analysis from './Analysis.tsx'
-import Footer from './Footer.tsx'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
     <Routes>
-      <Route path="/" element={<Navbar />}>
+      <Route path="/" element={<Layout  />}>
         <Route index element={<Home />} />
         <Route path="info" element={<Info />} />
         <Route path="analysis" element={<Analysis />} />
+        <Route path="*" element={<h1 className='m-5'>404 Not Found</h1>} />
       </Route>
 
     </Routes>
     </BrowserRouter>
-        <Footer/>
   </StrictMode>
 )
