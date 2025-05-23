665e78ee04244354b7c03b4fc2d8863909de38cb
michaelm1464@nycstudents.net <mmgbeojirikwe@gmail.com>
2025-05-23 14:56:50 -0400

    layout

--- client/src/Layout.tsx
+++ client/src/Layout.tsx
@@ -2,14 +2,14 @@
 import Navbar from './Navbar';
 import Footer from './Footer';
 import './App.css';
-import { Outlet } from 'react-router-dom';
+//import { Outlet } from 'react-router-dom';
 
 function Layout() {
   return (
     <>
       <Navbar />
       <main>
-        <Outlet />
+       
       </main>
         <Footer />
     </>
